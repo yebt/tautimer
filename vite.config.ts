c0a6f4dfb2cc463a4a99021257f855c6cfde9d94
Yahir Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2025-03-22 11:06:34 -0500

    feat: add `@` alias resolver

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "node:url";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  resolve:{
+    alias:{
+      "@": fileURLToPath(new URL('./src',import.meta.url ))
+    }
+  },
   plugins: [
     tailwindcss(),
     vue(),
